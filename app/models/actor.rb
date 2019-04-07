e733f74380d8f56b7c8a962a9291d1460ba21656
Max Brooks <maxbrooks114@gmail.com>
2019-04-07 22:49:09 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
+require "pry"
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name
+   name = self.first_name.split
+   name << self.last_name 
+   name.join(" ")
+  end
+  
+  def list_roles
+   roles = self.characters.first.name
+   show = self.shows.first.name
+   #binding.pry
+   r = roles.split
+   r << show 
+   r.join(" - ")
+  end
 end
