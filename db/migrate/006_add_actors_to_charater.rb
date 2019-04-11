f811510960eb1daa5d7a68d258b469c1633a1944
Nissan Holzer <nissan.holzer@gmail.com>
2019-04-11 22:30:35 +0000

    Done.

---
+++ db/migrate/006_add_actors_to_charater.rb
@@ -0,0 +1,3 @@
+class AddActorsToCharacter < ActiveRecord::Migration[4.2]
+
+end 
--- db/migrate/006_add_characters_to_actor.rb
+++
@@ -1,5 +0,0 @@
-class AddCharactersToActor < ActiveRecord::Migration[4.2] 
-  def change
-    add_column :actors, :character_id, :integer 
-  end
-end 
