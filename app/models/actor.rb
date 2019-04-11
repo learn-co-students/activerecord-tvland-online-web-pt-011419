d0ff382d436f19c5dfab17b1adf1b6848b41a3a7
Nissan Holzer <nissan.holzer@gmail.com>
2019-04-11 23:59:04 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,11 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
   
+  def full_name
+    "#{first_name} #{last_name}"
+  end 
+  
+  def list_roles
+    self.characters.each {|c| puts "#{c.name} - #{c.show.name}"}
+  end 
 end
